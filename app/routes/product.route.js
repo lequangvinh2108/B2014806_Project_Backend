4fb21519b49bee41cf9bb4942af286c4a972f3ae
lequangvinh2108 <vinhb2014806@student.ctu.edu.vn>
2023-11-16 22:53:38 +0700

    done - part 1

---
+++ app/routes/product.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const product = require("../controllers/product.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(product.findAll)
+    .post(product.create)
+    .delete(product.deleteAll);
+
+router.route("/favorite")
+    .get(product.findAllFavorite);
+
+router.route("/:id")
+    .get(product.findOne)
+    .put(product.update)
+    .delete(product.delete);
+
+module.exports = router;
